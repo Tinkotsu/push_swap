956e0bd7252eb4f8faf7abb3b260acb58561600a
Tinkotsu <53180682+Tinkotsu@users.noreply.github.com>
2019-10-29 19:02:07 +0300

    12

--- srcs/swap.c
+++ srcs/swap.c
@@ -19,13 +19,16 @@ void	sa(t_ps *s)
 
 void	sb(t_ps *s)
 {
-	t_st *temp;
+	t_st *temp1;
+	t_st *temp2;
 
 	if (s->stack_b && s->stack_b->next)
 	{
-		temp = s->stack_b->next;
-		s->stack_b->next = s->stack_b;
-		s->stack_b = temp;
+		temp1 = s->stack_b;
+		temp2 = s->stack_b->next;
+		temp1->next = temp2->next;
+		s->stack_b = temp2;
+		s->stack_b->next = temp1;
 	}
 }
 
