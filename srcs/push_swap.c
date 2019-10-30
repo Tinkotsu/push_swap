deec18aadc458bcacbb5f82d2f9ccdbab662f476
Tinkotsu <53180682+Tinkotsu@users.noreply.github.com>
2019-10-30 23:35:31 +0300

    12

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -2,6 +2,22 @@
 
 #include "push_swap.h"
 
+static void	sort_three(t_ps *s, int min, int max)
+{
+	if (!(is_sorted(s->stack_a) || is_r_sorted(s->stack_a)))
+	{
+		if (s->stack_a->n == max || s->stack_a->n == min)
+		{
+			ra(s);
+			write(1, "ra\n", 3);
+		}
+		else 
+		{
+			rra(s);
+			write(1, "rra\n", 4);
+		}
+	}
+}
 
 static void	leave_three(t_ps *s)
 {
@@ -14,15 +30,21 @@ static void	leave_three(t_ps *s)
 	len = stack_len(s->stack_a);
 	while (len > 3)
 	{
-		while (s->stack_a->n == min || s->stack_a->n == max)
+		if (s->stack_a->n == min)
+		{
+			ra(s);
+			write(1, "ra\n", 3);
+		}
+		if (s->stack_a->n == max)
 		{
 			ra(s);
 			write(1, "ra\n", 3);
 		}
 		pb(s);
 		write(1, "pb\n", 3);
-		--len;
+		len--;
 	}
+	sort_three(s, min, max);
 }
 
 void		push_swap(t_ps *s)
