95f4370a485d2e564d32dca38e71c9fd8a3645a4
Irena Fran <ifran@ga-k3.21-school.ru>
2019-11-02 18:51:16 +0300

    12

--- srcs/leave_three.c
+++ srcs/leave_three.c
@@ -2,7 +2,7 @@
 
 #include "push_swap.h"
 
-static void	sort_three(t_ps *s, int min, int max)
+static void	sort_three(t_ps *s, int min, int max) //оставляю max min val
 {
 	if (s->stack_a->n == min)
 	{
@@ -31,6 +31,33 @@ static void	sort_three(t_ps *s, int min, int max)
 	}
 }
 
+static void	from_b_to_a(t_ps *s, size_t len, int min, int max)
+{
+	int		head;
+	int		tail;
+	t_st	*temp;
+
+	while (len > 3)
+	{
+		while (s->stack_a->n == min || s->stack_a->n == max)
+			ra(s);
+		temp = s->stack_b;
+		if (!s->stack_b || s->stack_a->n > s->stack_b->n)
+		{
+			pb(s);
+			head = s->stack_b->n;
+			while (temp->next)
+				temp = temp->next;
+			tail = temp->n; 
+		}
+		else // число а меньше, чем б
+		{
+			;
+		}
+		len--;
+	}
+}
+
 void		leave_three(t_ps *s)
 {
 	size_t	len;
@@ -41,12 +68,6 @@ void		leave_three(t_ps *s)
 	max = find_max(s->stack_a);
 	if (!(len = stack_len(s->stack_a)))
 		return ;
-	while (len > 3)
-	{
-		while (s->stack_a->n == min || s->stack_a->n == max)
-			ra(s);
-		pb(s);
-		len--;
-	}
+	from_b_to_a(s, len, min, max);
 	sort_three(s, min, max);
 }
