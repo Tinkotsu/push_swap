99af7b99d777bfdb04232263538580731916b6b4
Tinkotsu <53180682+Tinkotsu@users.noreply.github.com>
2019-11-04 17:59:40 +0300

    12

--- srcs/leave_three.c
+++ srcs/leave_three.c
@@ -46,33 +46,6 @@ void		do_index(t_st *stack)
 	}
 }
 
-static void	from_b_to_a(t_ps *s, size_t len, int min, int max)
-{
-	int		head;
-	int		tail;
-	t_st	*temp;
-
-	while (len > 3)
-	{
-		while (s->stack_a->n == min || s->stack_a->n == max)
-			ra(s);
-		temp = s->stack_b;
-		if (!s->stack_b || s->stack_a->n > s->stack_b->n)
-		{
-			pb(s);
-			head = s->stack_b->n;
-			while (temp->next)
-				temp = temp->next;
-			tail = temp->n; 
-		}
-		else // число а меньше, чем б
-		{
-			;
-		}
-		len--;
-	}
-}
-
 void		leave_three(t_ps *s)
 {
 	size_t	len;
@@ -83,7 +56,6 @@ void		leave_three(t_ps *s)
 	max = find_max(s->stack_a);
 	if (!(len = stack_len(s->stack_a)))
 		return ;
-<<<<<<< HEAD
 	while (len > 3) //или если стек А отсортирован
 	{
 		while (s->stack_a->n == min || s->stack_a->n == max)
@@ -95,8 +67,3 @@ void		leave_three(t_ps *s)
 	do_index(s->stack_a);
 	do_index(s->stack_b);
 }
-=======
-	from_b_to_a(s, len, min, max);
-	sort_three(s, min, max);
-}
->>>>>>> 95f4370a485d2e564d32dca38e71c9fd8a3645a4
