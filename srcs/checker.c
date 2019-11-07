9529dcc217a11a0e97b7643da22965e9cdd9aa32
Irena Fran <ifran@il-e4.21-school.ru>
2019-11-07 21:16:05 +0300

    12

--- srcs/checker.c
+++ srcs/checker.c
@@ -6,35 +6,35 @@
 /*   By: ifran <ifran@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 18:02:11 by ifran             #+#    #+#             */
-/*   Updated: 2019/11/07 19:44:22 by ifran            ###   ########.fr       */
+/*   Updated: 2019/11/07 21:14:36 by ifran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-static void	check_op(t_ps *s, char *str, int i)
+static void	check_op(t_ps *s, char *str)
 {
-	if (i == 3 && ft_strequ(str, "pa\n"))
+	if (ft_strequ(str, "pa\n"))
 		pa(s, 0);
-	else if (i == 3 && ft_strequ(str, "pb\n"))
+	else if (ft_strequ(str, "pb\n"))
 		pb(s, 0);
-	else if (i == 3 && ft_strequ(str, "sa\n"))
+	else if (ft_strequ(str, "sa\n"))
 		sa(s, 0);
-	else if (i == 3 && ft_strequ(str, "sb\n"))
+	else if (ft_strequ(str, "sb\n"))
 		sb(s, 0);
-	else if (i == 3 && ft_strequ(str, "ss\n"))
+	else if (ft_strequ(str, "ss\n"))
 		ss(s, 0);
-	else if (i == 3 && ft_strequ(str, "ra\n"))
+	else if (ft_strequ(str, "ra\n"))
 		ra(s, 0);
-	else if (i == 3 && ft_strequ(str, "rb\n"))
+	else if (ft_strequ(str, "rb\n"))
 		rb(s, 0);
-	else if (i == 3 && ft_strequ(str, "rr\n"))
+	else if (ft_strequ(str, "rr\n"))
 		rr(s, 0);
-	else if (i == 4 && ft_strequ(str, "rra\n"))
+	else if (ft_strequ(str, "rra\n"))
 		rra(s, 0);
-	else if (i == 4 && ft_strequ(str, "rrb\n"))
+	else if (ft_strequ(str, "rrb\n"))
 		rrb(s, 0);
-	else if (i == 4 && ft_strequ(str, "rrr\n"))
+	else if (ft_strequ(str, "rrr\n"))
 		rrr(s, 0);
 	else
 		error();
@@ -43,25 +43,18 @@ static void	check_op(t_ps *s, char *str, int i)
 void		checker(t_ps *s)
 {
 	char	*buff;
-	int		i;
 
-	i = 0;
 	if (!(buff = ft_strnew(4)))
 		error();
-	while (read(0, buff, 1))
+	while (read(0, buff, 4))
 	{
-		if (i > 4)
-			error();
-		if (i > 2 && ft_strchr(buff, '\n'))
+		if (ft_strchr(buff, '\n'))
 		{
-			check_op(s, buff, i);
+			check_op(s, buff);
 			ft_strclr(buff);
-			i = 0;
 		}
-		++i;
 	}
-	if (i)
-		error();
+	free(buff);
 	if (!s->stack_b && is_sorted(s->stack_a))
 		write(1, "OK\n", 3);
 	else
